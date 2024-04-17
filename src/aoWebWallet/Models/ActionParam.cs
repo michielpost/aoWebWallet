62704633abbc631221bfc5d631326127cde7c92e
Michiel Post <michiel@michielpost.nl>
2024-04-17 14:11:15 +0200

    First setup for Action Page

---
+++ src/aoWebWallet/Models/ActionParam.cs
@@ -0,0 +1,44 @@
+﻿namespace aoWebWallet.Models
+{
+    public class AoAction
+    {
+        public List<ActionParam> Params { get; set; } = new();
+
+        public ActionParam? Target => Params.Where(x => x.ParamType == ActionParamType.Target).FirstOrDefault();
+        public IEnumerable<ActionParam> Filled => Params.Where(x => x.ParamType == ActionParamType.Filled);
+        public IEnumerable<ActionParam> AllInputs => Params.Where(x => 
+                                                        x.ParamType != ActionParamType.Filled
+                                                        && x.ParamType != ActionParamType.Target);
+
+        public string? IsValid()
+        {
+            if (Target == null)
+                return "No Target process specified.";
+
+            return null;
+        }
+    }
+
+    public class ActionParam
+    {
+        public required string Key { get; set; }
+        public string? Value { get; set; }
+
+        public string? TokenId { get; set; }
+
+        public ActionParamType ParamType { get; set; }
+
+    }
+
+    public enum ActionParamType
+    {
+        None = 0,
+        Target,
+        Filled,
+        Input,
+        Integer,
+        Process,
+        Balance, //Must have balance
+        Quantity, //Does not care about balance
+    }
+}
---
+++ src/aoWebWallet/Pages/ActionPage.razor.cs
@@ -0,0 +1,97 @@
+﻿using aoWebWallet.Models;
+using aoWebWallet.ViewModels;
+using Microsoft.AspNetCore.Components.Routing;
+using System.Net;
+
+namespace aoWebWallet.Pages
+{
+    public partial class ActionPage : MvvmComponentBase<MainViewModel>
+    {
+        public AoAction AoAction { get; set; } = new();
+
+        protected override void OnInitialized()
+        {
+            GetQueryStringValues();
+            WatchDataLoaderVM(BindingContext.TokenList);
+            NavigationManager.LocationChanged += NavigationManager_LocationChanged;
+        }
+
+        private void NavigationManager_LocationChanged(object? sender, LocationChangedEventArgs e)
+        {
+            GetQueryStringValues();
+            StateHasChanged();
+        }
+
+        void GetQueryStringValues()
+        {
+            var uri = new Uri(NavigationManager.Uri);
+            var query = uri.Query;
+
+            // Parsing query string
+            var queryStringValues = System.Web.HttpUtility.ParseQueryString(query);
+
+            AoAction = new AoAction();
+
+            foreach (var key in queryStringValues.AllKeys)
+            {
+                if (key == null)
+                    continue;
+
+                var values = queryStringValues.GetValues(key);
+                if (values == null || !values.Any())
+                    continue;
+
+                foreach(var val in values)
+                {
+                    string actionKey = key;
+                    string? actionValue = val.ToString();
+                    ActionParamType actionParamType = ActionParamType.Filled;
+
+                    if (key.Equals("Target", StringComparison.InvariantCultureIgnoreCase))
+                        actionParamType = ActionParamType.Target;
+                    if (key.Equals("X-Quantity", StringComparison.InvariantCultureIgnoreCase))
+                        actionParamType = ActionParamType.Quantity;
+                    else if (key.Equals("X-Process", StringComparison.InvariantCultureIgnoreCase))
+                        actionParamType = ActionParamType.Process;
+                    else if (key.Equals("X-Int", StringComparison.InvariantCultureIgnoreCase))
+                        actionParamType = ActionParamType.Integer;
+                    else if (key.Equals("X-Input", StringComparison.InvariantCultureIgnoreCase))
+                        actionParamType = ActionParamType.Input;
+
+                    if (actionParamType != ActionParamType.Filled
+                        && actionParamType != ActionParamType.Target)
+                    {
+                        actionKey = val;
+                        actionValue = null;
+                    }
+
+                    AoAction.Params.Add(new ActionParam
+                    {
+                        Key = actionKey,
+                        Value = actionValue,
+                        ParamType = actionParamType
+                    });
+
+                }
+            }
+
+            StateHasChanged();
+        }
+
+       
+
+        public void Dispose()
+        {
+            NavigationManager.LocationChanged -= NavigationManager_LocationChanged;
+        }
+
+        protected override async Task LoadDataAsync()
+        {
+            await BindingContext.LoadTokenList();
+
+            await base.LoadDataAsync();
+
+        }
+
+    }
+}
