85a29d9f5eddcd17d2c0fe553388f106cbb3dc09
Michiel Post <michiel@michielpost.nl>
2024-05-29 17:48:08 +0200

    Poll for new transactions on receive page

--- src/aoWebWallet/ViewModels/ReceiveViewModel.cs
+++ src/aoWebWallet/ViewModels/ReceiveViewModel.cs
@@ -1,16 +1,26 @@
 ﻿using aoWebWallet.Models;
 using aoWebWallet.Services;
+using aoww.Services;
+using aoww.Services.Models;
+using ArweaveAO;
 using CommunityToolkit.Mvvm.ComponentModel;
 using System.Net;
+using System.Text;
+using webvNext.DataLoader;
 
 namespace aoWebWallet.ViewModels
 {
-    public partial class ReceiveViewModel(TokenDataService tokenDataService) : ObservableObject
+    public partial class ReceiveViewModel(TokenDataService tokenDataService,
+        GraphqlClient graphqlClient
+        ) : ObservableObject
     {
         [ObservableProperty]
         private Token? _token;
 
+        public DataLoaderViewModel<List<TokenTransfer>> TokenTransferList { get; set; } = new();
+
         public string? TokenId { get; set; }
+        public DateTimeOffset StartTime { get; set; } = DateTimeOffset.Now;
         public required string Address { get; set; }
         public string QrCode
         {
@@ -43,12 +53,34 @@ namespace aoWebWallet.ViewModels
             Address = address;
             Token = null;
             TokenId = tokenId;
+            StartTime = DateTimeOffset.UtcNow;
+            TokenTransferList.Data?.Clear();
 
             if (!string.IsNullOrWhiteSpace(tokenId))
             {
                 Token = await tokenDataService.LoadTokenAsync(tokenId);
             }
 
+            await LoadTokenTransferList();
         }
+
+        public Task LoadTokenTransferList() => TokenTransferList.DataLoader.LoadAsync(async () =>
+        {
+            var address = Address;
+
+            var incoming = await graphqlClient.GetTransactionsIn(address);
+
+            incoming = incoming.Where(x => x.Timestamp > StartTime).OrderByDescending(x => x.Timestamp).ToList();
+
+            if(!string.IsNullOrEmpty(TokenId))
+                incoming = incoming.Where(x => x.TokenId == TokenId).ToList();
+
+            TokenTransferList.Data = incoming.OrderByDescending(x => x.Timestamp).ToList();
+
+            List<string> allTokenIds = incoming.Where(x => x.TokenId != null).Select(x => x.TokenId!).Distinct().ToList();
+            tokenDataService.TryAddTokenIds(allTokenIds);
+
+            return TokenTransferList.Data;
+        });
     }
 }
